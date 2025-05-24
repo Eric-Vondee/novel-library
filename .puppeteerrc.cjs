2625ccd6829405b5ae2417f083cdabbd3cc1a00a
Greg-Soap <emmanuelkingsley36@gmail.com>
2025-05-24 23:21:53 +0100

    fix #4

--- .puppeteerrc.cjs
+++
@@ -1,9 +0,0 @@
-const { join } = require('node:path')
-
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-  // Use a path that will be included in the deployment
-  cacheDirectory: join(process.cwd(), 'public', '.cache', 'puppeteer'),
-}
